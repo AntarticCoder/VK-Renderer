c222bc0e5fb40a3432c686d0a839e2c54a16c90c
Antarctic Coder <Nitishpraks2009@gmail.com>
2023-05-29 12:25:08 -0400

    Renderpasses
    🌋Update: This commit adds vulkan renderpasses to the equation. At the moment the renderpasses are hard coded values, however in the future, it might become configureable.

---
+++ src/graphics-pipeline/vk_renderpass.cpp
@@ -0,0 +1,43 @@
+#include <graphics-pipeline/vk_renderpass.h>
+#include <utils/vk_utils.h>
+
+void VulkanRenderPass::CreateRenderpass()
+{
+    VkAttachmentDescription colorAttachment{};
+    colorAttachment.format = swapchain->GetFormat();
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachmentRef{};
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass{};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentRef;
+
+    VkRenderPassCreateInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    VkResult result = vkCreateRenderPass(device->GetLogicalDevice(), &renderPassInfo, nullptr, &renderPass);
+    VK_CHECK(result);
+}
+
+void VulkanRenderPass::Destroy()
+{
+    vkDestroyRenderPass(device->GetLogicalDevice(), renderPass, nullptr);
+}
---
+++ src/graphics-pipeline/vk_renderpass.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <app-context/vk_device.h>
+#include <app-context/vk_swapchain.h>
+
+class VulkanRenderPass
+{  
+private:
+    VulkanDevice* device;
+    VulkanSwapchain* swapchain;
+
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+
+    bool initialized = false;
+public:
+    VulkanRenderPass(VulkanDevice* device, VulkanSwapchain* swapchain) : device(device), swapchain(swapchain) {}
+    ~VulkanRenderPass()
+    {
+        if(initialized) { Destroy(); }
+    }
+
+    void CreateRenderpass();
+    void Destroy();
+};
