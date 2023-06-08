e65ee38fdeafc08dd65eb02e1211df94fe898dc0
Antarctic Coder <Nitishpraks2009@gmail.com>
2023-06-08 19:16:17 -0400

    Removed hardcoded Vulkan SDK Paths and Updated the github action
    🌋Removed the Vulkan SDK paths that are hardcoded in build.sh Renamed the GitHub action and steps for the job.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 clear
 
-export VULKAN_SDK=/Users/nick/VulkanSDK/1.3.243.0/macOS
-export VK_LAYER_PATH=/Users/nick/VulkanSDK/1.3.243.0/macOS/share/vulkan/explicit_layer.d
-export VK_ICD_FILENAMES=/Users/nick/VulkanSDK/1.3.243.0/macOS/share/vulkan/icd.d/MoltenVK_icd.json
+# export VULKAN_SDK=/Users/nick/VulkanSDK/1.3.243.0/macOS
+# export VK_LAYER_PATH=/Users/nick/VulkanSDK/1.3.243.0/macOS/share/vulkan/explicit_layer.d
+# export VK_ICD_FILENAMES=/Users/nick/VulkanSDK/1.3.243.0/macOS/share/vulkan/icd.d/MoltenVK_icd.json
 
 sh compile-shaders.sh
 
